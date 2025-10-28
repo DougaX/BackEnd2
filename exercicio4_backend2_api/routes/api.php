2f03aa1172a9ef6ab17796e441eb7a35f40dcdc1
DougaX <viniciusbduarte@gmail.com>
2025-10-28 19:44:12 -0300

    Configurando rotas explícitas da API e habilita carregamento de api.php

--- exercicio4_backend2_api/routes/api.php
+++ exercicio4_backend2_api/routes/api.php
@@ -5,11 +5,23 @@ use App\Http\Controllers\Api\ProfessorApiController;
 use App\Http\Controllers\Api\AdministradorApiController;
 use App\Http\Controllers\Api\SalaApiController;
 
-// Rotas API para Professores
-Route::apiResource('professores', ProfessorApiController::class);
+// Professores
+Route::get('professores', [ProfessorApiController::class, 'index']);
+Route::post('professores', [ProfessorApiController::class, 'store']);
+Route::get('professores/{id}', [ProfessorApiController::class, 'show']);
+Route::put('professores/{id}', [ProfessorApiController::class, 'update']);
+Route::delete('professores/{id}', [ProfessorApiController::class, 'destroy']);
 
-// Rotas API para Administradores
-Route::apiResource('administradores', AdministradorApiController::class);
+// Administradores
+Route::get('administradores', [AdministradorApiController::class, 'index']);
+Route::post('administradores', [AdministradorApiController::class, 'store']);
+Route::get('administradores/{id}', [AdministradorApiController::class, 'show']);
+Route::put('administradores/{id}', [AdministradorApiController::class, 'update']);
+Route::delete('administradores/{id}', [AdministradorApiController::class, 'destroy']);
 
-// Rotas API para Salas
-Route::apiResource('salas', SalaApiController::class);
+// Salas
+Route::get('salas', [SalaApiController::class, 'index']);
+Route::post('salas', [SalaApiController::class, 'store']);
+Route::get('salas/{id}', [SalaApiController::class, 'show']);
+Route::put('salas/{id}', [SalaApiController::class, 'update']);
+Route::delete('salas/{id}', [SalaApiController::class, 'destroy']);
